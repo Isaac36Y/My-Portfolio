aa4bb28ddd348f27d15fbe8c2779d2b7d9f8cf46
Isaac Young <isaac3.young6@gmail.com>
2026-05-28 09:57:58 -0700

    set up for IntersectionObserver and markers

--- src/components/home/MySkills.tsx
+++ src/components/home/MySkills.tsx
@@ -1,23 +1,45 @@
 import styles from './MySkills.module.scss'
 
+const skillLists = [
+    {
+        label: '<frontend>',
+        id: 'frontend',
+        skills: ['Semantic HTML','Accessibility', 'CSS', 'SCSS', 'Responsive Design', 'JavaScript', 'TypeScript', 'React', 'Next.js',
+                'Animations', 'Canvas API', 'AI Integration']
+    },
+    {
+        label: '{ backend }',
+        id: 'backend',
+        skills: []
+    },
+    {
+        label: 'tools',
+        id: 'tools',
+        skills: []
+    }
+]
+
 function Title() {
     return (
         <h2 className={`${ styles.title } primary-text`}>
-            My Skills
+            Skills and Tools
         </h2>
     )
 }
 
-function SkillCard() {
+function SkillCards() {
     return (
-        <div className={ styles.skillCard }>
-            <h3 className={`${ styles.skillType } tech secondary-text`}>frontend</h3>
-            <ul className={`${ styles.skillList } body accent-text`} role='list'>
-                <li>JavaScript</li>
-                <li>Semantic HTML</li>
-                <li>CSS</li>
-                <li>SCSS</li>
-            </ul>
+        <div className={ styles.skillCarousel}>
+            { skillLists.map((card, i) => (
+                <div className={ styles.skillCard } key={i}>
+                    <h3 className={`${ styles.skillType } tech secondary-text`}>{ card.label }</h3>
+                    <ul className={`${ styles.skillList } body primary-text`} role='list' id='frontend-skill-list'>
+                        { card.skills.map((skill, i) => (
+                            <li key={i}>{skill}</li>
+                        ))}
+                    </ul>
+                </div>
+            )) }
         </div>
     )
 } 
@@ -28,7 +50,7 @@ export default function MySkills() {
     return (
         <section className={ styles.section }>
             <Title />
-            <SkillCard />
+            <SkillCards />
         </section>
     )
 } 

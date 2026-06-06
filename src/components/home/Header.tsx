8a589dd323bf27e5f7f9c7d03f6519bd3b04ad68
Isaac Young <isaac3.young6@gmail.com>
2026-06-06 16:27:18 -0700

    Refactor(About me): Changed the bio and my recents
    took out the bio because it was kinda basic text every dev has on their portfolio. Left a more simple status with all the recent apis so it still tells a story without so much noise

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -1,14 +1,13 @@
 import styles from './Header.module.scss'
 import Link from 'next/link'
 import filterStravaActivities from '@/lib/strava'
+import { IconRun, IconCode, IconBook } from '@tabler/icons-react';
 
 function Hero() {
     return (
         <div className={styles.hero}>
             <h1 className={`${styles.heroName} primary-text`}>Isaac <span className={`${styles.lastName} secondary-text`}>Young.</span></h1>
             <p className={`${styles.description} tech secondary-text`}>// software engineer & creative dev</p>
-            <p className={`${styles.bio} body primary-text`}>I'm a self taught, full-stack developer, eager to learn more everyday. I enjoy audiobooks, running and lifting, and building fun,
-            useful things for the web</p>
         </div>
     )
 }
@@ -18,6 +17,60 @@ function NavButton({ link, description }: { link: string, description: string })
         <Link href={link} className={`${styles.headerNavBtn} body accent-text`}>
             {description}
         </Link>
+    )
+}
+
+
+
+function CurrentRead() {
+    return (
+        <div className={`${styles.status} body primary-text`}>
+            <div className={`${styles.label}`}>
+                <IconBook stroke={ 1.75 } size={ 24 } color='var(--color-primary)' />
+                <p className='body primary-text'>listening</p>
+            </div>
+            <a href='' className={styles.currentRead}>The Midnight Library<br/>by Matt Haig</a>
+        </div>
+        
+    )
+}
+
+async function WeeklyMiles() {
+    const last7Activities = await filterStravaActivities()
+    const meters =  last7Activities.reduce((acc: number, curr: { distance: number}) => acc + curr.distance, 0)
+    const miles = (meters / 1609).toFixed(1)
+ 
+    return (
+        <div className={`${styles.status} body primary-text`}>
+            <div className={`${styles.label}`}>
+                <IconRun stroke={ 1.75 } size={ 24 } color='var(--color-primary)' />
+                <p>running</p>
+            </div>
+            <a href='https://www.strava.com/athletes/125614194' className={styles.milesLink}>{miles} miles this week</a>
+        </div>
+    )
+}
+
+function CurrentWork() {
+    return (
+        <div className={`${styles.status} body primary-text`}>
+            <div className={`${styles.label}`}>
+                <IconCode stroke={ 1.75 } size={ 24 } color='var(--color-primary)' />
+                <p>building </p>
+            </div>
+            <a href='' className={ styles.currentRead }>my-portfolio</a>
+        </div>
+    )
+}
+
+function AboutMeRecents() {
+    return (
+        <div className={styles.aboutMeRecents}>
+            <CurrentWork />
+            <WeeklyMiles />
+            <CurrentRead />
+        </div>
+        
     )
 }
 
@@ -31,44 +84,13 @@ function MobileNav() {
     )
 }
 
-function CurrentRead() {
-    return (
-        <p className='body primary-text'>I am currently listening to <a href='' className={styles.currentRead}>The Midnight Library by Matt Haig</a></p>
-    )
-}
-
-async function WeeklyMiles() {
-    const last7Activities = await filterStravaActivities()
-    const meters =  last7Activities.reduce((acc: number, curr: { distance: number}) => acc + curr.distance, 0)
-    const miles = (meters / 1609).toFixed(1)
- 
-    return (
-        <p className='body primary-text'>This week I've logged <a href='https://www.strava.com/athletes/125614194' className={styles.milesLink}>{miles} miles</a></p>
-    )
-}
-
-function CurrentWork() {
-    return (
-        <p className='body primary-text'>The last project I worked on was <a href='' className={styles.currentRead}>my-portfolio</a></p>
-    )
-}
-
-function AboutMeRecents() {
-    return (
-        <div className={styles.aboutMeRecents}>
-            <CurrentRead />
-            <WeeklyMiles />
-            <CurrentWork />
-        </div>
-        
-    )
-}
-
 export default function Header() {
     return (
         <header className={styles.header}>
             <Hero />
+            <hr className={`${styles.line}`} />
             <AboutMeRecents />
+            <hr className={`${styles.line}`} />
             <MobileNav />
         </header>
     )

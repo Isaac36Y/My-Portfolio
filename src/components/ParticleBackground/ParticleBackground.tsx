fb824f2b551101492178974dce33649bb6c7075c
Isaac Young <isaac3.young6@gmail.com>
2026-06-19 08:11:58 -0700

    tightening effect works for the particles

--- src/components/ParticleBackground/ParticleBackground.tsx
+++ src/components/ParticleBackground/ParticleBackground.tsx
@@ -1,10 +1,23 @@
 'use client'
 
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState, useContext } from 'react';
 import styles from './ParticleBackground.module.scss'
+import { TransitionContext } from '../NavLogic/Provider';
 
 export default function ParticleBackground() {
     const canvasRef = useRef<HTMLCanvasElement>(null)
+    const toGo = useRef<{ x: number, vx: number, y: number, vy: number, radius: number }[]>([])
+    const { exiting } = useContext(TransitionContext)
+    const exitingRef = useRef<boolean>(false)
+    const newParticleRef = useRef<boolean>(false)
+    const lerp = (start: number, end: number, t: number) => start + (end - start) * t;
+
+    useEffect(() => {
+        exitingRef.current = exiting
+        newParticleRef.current = exiting
+        toGo.current = []
+    }, [exiting])
+
     useEffect(() => {
         const canvas = canvasRef.current
         if (!canvas) return 
@@ -19,15 +32,20 @@ export default function ParticleBackground() {
         const particles: { x: number, vx: number, y: number, vy: number, radius: number }[] = []
         
         for (let i = 0; i < particleAmount; i++ ) {
-            particles.push({ 
-                x: Math.random() * (canvas.width - 4),
-                vx: Math.random() * 2 - 1,
-                y: Math.random() * (canvas.height - 4),
-                vy: Math.random() * 2 - 1,
-                radius: 4 })
+        particles.push({ 
+            x: Math.random() * (canvas.width - 4),
+            vx: Math.random() * 2 - 1,
+            y: Math.random() * (canvas.height - 4),
+            vy: Math.random() * 2 - 1,
+            radius: 4 })
         }
 
         let animId: number;
+
+        const tightenRect = {
+            top: 50,
+            bottom: 300
+        }
 
         function draw() {
             ctx!.clearRect(0, 0, canvas!.width, canvas!.height)
@@ -37,22 +55,61 @@ export default function ParticleBackground() {
             ctx!.strokeStyle = strokeColor
             ctx!.save()
 
-            for (let particle of particles ) {
-                particle.x = particle.x + particle.vx
-                particle.y = particle.y + particle.vy
-
-                if (particle.x + particle.vx > canvas!.width - particle.radius || particle.x + particle.vx < particle.radius) {
-                    particle.vx = -particle.vx
+            if (exitingRef.current) {
+                if (newParticleRef.current) {
+                    for (let i = 0; i < particles.length; i++ ) {
+                        const tightenHeight = tightenRect.bottom - tightenRect.top
+                        const newPart = { 
+                            x: particles[i].x, 
+                            vx: particles[i].vx,
+                            y: ((particles[i].y * tightenHeight) / canvas!.height) + tightenRect.top,
+                            vy: particles[i].vy,
+                            radius: 4 
+                        }
+                        toGo.current.push(newPart)
+                    }
+                    newParticleRef.current = false
                 }
-                if (particle.y + particle.vy > canvas!.height - particle.radius || particle.y + particle.vy < particle.radius) {
-                    particle.vy = -particle.vy
-                }
 
+                for (let i = 0; i < toGo.current.length; i++) {
+                    particles[i].x = particles[i].x + particles[i].vx
+
+
+                    if (particles[i].y > toGo.current[i].y) {
+                        particles[i].y = lerp(particles[i].y, toGo.current[i].y, 0.05)
+                    }
+                    if (particles[i].y < toGo.current[i].y) {
+                        particles[i].y = lerp(particles[i].y, toGo.current[i].y, 0.05)
+                    }
+
+                    if (particles[i].x + particles[i].vx > canvas!.width - particles[i].radius || particles[i].x + particles[i].vx < particles[i].radius) {
+                        particles[i].vx = -particles[i].vx
+                    }
+                    
                 ctx!.beginPath()
-                ctx!.arc(particle.x, particle.y, particle.radius, 0, Math.PI * 2)
+                ctx!.arc(particles[i].x, particles[i].y, particles[i].radius, 0, Math.PI * 2)
                 ctx!.fill()
+                }
+            }else {
+                for (let particle of particles ) {
+                    particle.x = particle.x + particle.vx
+                    particle.y = particle.y + particle.vy   
+
+                    if (particle.x + particle.vx > canvas!.width - particle.radius || particle.x + particle.vx < particle.radius) {
+                        particle.vx = -particle.vx
+                    }
+                    if (particle.y + particle.vy > canvas!.height - particle.radius || particle.y + particle.vy < particle.radius) {
+                        particle.vy = -particle.vy
+                    }
+
+                    ctx!.beginPath()
+                    ctx!.arc(particle.x, particle.y, particle.radius, 0, Math.PI * 2)
+                    ctx!.fill()
+                }
             }
 
+            
+        
             ctx!.beginPath()
             particles.forEach((particle, index) => {
                 for (let i = index + 1; i < particles.length; i++) {
@@ -65,10 +122,10 @@ export default function ParticleBackground() {
                 }
             })
             ctx!.stroke()
-            
             animId = requestAnimationFrame(draw);
+            
         }
-        animId = requestAnimationFrame(draw);
+        animId = requestAnimationFrame(draw)
         return () => cancelAnimationFrame(animId)
         
     }, [])
